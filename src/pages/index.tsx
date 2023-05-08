7b4a438f4e5b71363e76deb70c5774f412823713
rarma4 <rarma4@gmail.com>
2023-05-08 00:31:41 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        .. Rafart ..
+        .. Rafart ....
       </div>
     </main>
   )
