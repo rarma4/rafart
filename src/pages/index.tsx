dec0da288ec4fb25e06d50ca8c34dede941c995d
rarma4 <rarma4@gmail.com>
2023-06-09 23:32:22 -0300

    teste hader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Header from '../components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        .. Rafart .... init
-      </div>
-    </main>
+    <>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-4`}
+        >
+        <Header></Header>
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          .. Rafart .... init
+        </div>
+      </main>
+    </>
   )
 }
