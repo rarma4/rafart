9812958f7cf4753c7234bea5b77fbd9edce5e8f7
rarma4 <rarma4@gmail.com>
2023-06-10 22:06:52 -0300

    testes cor e fonte

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,11 @@ export default function Home() {
   return (
     <>
       <main
-        className={`flex min-h-screen flex-col items-center justify-between p-4`}
+        className={`flex min-h-screen flex-col items-center p-4 bg-slate-900 text-gray-100`}
         >
         <Header></Header>
-        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          .. Rafart .... init
+        <div className="z-10 w-full max-w-6xl items-center justify-between font-sans text-lg lg:flex">
+          .. Rafart .... 
         </div>
       </main>
     </>
