2a0cb2cb338d99004eba61c4d65be0eb1f934a35
rarma4 <rarma4@gmail.com>
2023-10-16 22:56:59 -0300

    init components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.scss'
-
+import Navigation from './components/Navigation'
 
 export default function Home() {
   return (
     <>
-
-
-            <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-
-              <div className="container">
-                <a className="navbar-brand" href="#">
-                  <Image
-                    src="../../assets/img/rafart-logo.svg"
-                    alt="logo rafart"
-                    width={30}
-                    height={24} 
-                  />
-                </a>
-                <a className="navbar-brand" href="#">Raf Art</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarColor01">
-                  <ul className="navbar-nav ms-md-auto">
-                    <li className="nav-item">
-                      <a className="nav-link active" href="#">Home
-                        <span className="visually-hidden">(current)</span>
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">Features</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">About</a>
-                    </li>
-                    
-                  </ul>
-
-                </div>
-              </div>
-            </nav>
-
+      <Navigation/>
     </>
   )
 }
