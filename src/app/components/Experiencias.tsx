450fe9d7c7ce7e159f51b848480d6aa7c526f033
rarma4 <rarma4@gmail.com>
2023-12-27 16:14:28 -0300

    acerto experiencia

--- src/app/components/Experiencias.tsx
+++ src/app/components/Experiencias.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import '../styles/components/experiencia.scss'
+import '../styles/components/experiencias.scss'
 import { ExperienciaProfissional } from '../utils/Utils'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -15,19 +15,24 @@ export default function Experiencias() {
 
           <div className="accordion-item" key={index}>
             <h2 className="accordion-header" id="headingOne">
-              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={item.chave}aria-expanded="false" aria-controls={item.retorno}>
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={item.chave} aria-expanded="false" aria-controls={item.retorno}>
                 {item.nome}
               </button>
             </h2>
             <div id={item.retorno} className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
               <div className="accordion-body">
                 <strong>{item.descricao}</strong>
-                <p>{item.data}</p>
+                <p><code>{item.data}</code></p>
+                <ul>
+                  {item.detalhes.map((detalhe, index) => (
+                    <li key={index}>{detalhe}</li>
+                  ))}
+                </ul>
               </div>
             </div>
           </div>
 
-          
+
         ))}
 
         {/* <div className="accordion-item">
--- src/app/utils/Utils.tsx
+++ src/app/utils/Utils.tsx
@@ -3,7 +3,7 @@ export const FormacaoAcademica = [
       "id": 1,
       "nome": "Anhanguera Educacional",
       "data": "2015 - 2018",
-      "descricao": "Bacharel em Sistemas de Informação, conhecimentos em PHP, Javascript, Python, HTML, CSS, Bootstrap, WordPress, SQL, React. ",
+      "descricao": "Bacharel em Sistemas de Informação,\n conhecimentos em PHP, Javascript, Python, HTML, CSS, Bootstrap, WordPress, SQL, React. ",
     }
   ]
 
@@ -14,7 +14,10 @@ export const FormacaoAcademica = [
       "retorno":"one",
       "nome": "Tenda Negócios Imobiliários",
       "data": "10/2021 – até presente data",
-      "descricao": "Programador de Internet Web II",
+      "descricao": "Programador de Internet Web III",
+      "detalhes":[
+        "Implementação e testes de novas ferramentas para software (PHP, javascript, Jquery, Ajax, CSS, Html, C#, WordPress)",
+      ]
     },
     {
       "id": 2,
@@ -22,14 +25,35 @@ export const FormacaoAcademica = [
       "retorno":"two",
       "nome": "Spread Teleinformática",
       "data": "09/2020 – 09/2021",
-      "descricao": "Programador Frontend  junior",
+      "descricao": "Programador Frontend junior",
+      "detalhes": [
+        "Prestação de serviços para as empresas Novartis Pharma (09/2020 - 02/2021) e Qualicorp (03/2021 – 08/2021)",
+        "Implementação e testes de novas ferramentas para software WordPress (PHP, CSS, Html, Hubspot, Bootstrap, API, temas e Plugins)",
+        "Desenvolvimento e implementação de website na plataforma wordpress (temas e Plugins)/Hubspot"
+      ]
     },
     {
       "id": 3,
       "chave":"#three",
       "retorno":"three",
-      "nome": "Adin Comercial e serviços de Marketing",
+      "nome": "Adin Com. e Serv. de Marketing",
       "data": "09/2019 – 04/2020",
       "descricao": "Programador PHP junior",
+      "detalhes":[
+        "Implementação e testes de novas ferramentas para software (PHP, Jquery, Ajax, CSS, Html)",
+        "Desenvolvimento e implementação de website na plataforma wordpress/Magento"
+      ]
+    },
+    {
+      "id": 4,
+      "chave":"#four",
+      "retorno":"four",
+      "nome": "Dimensions Software Ltda",
+      "data": "04/2019 – 08/2019",
+      "descricao": "Assistente de desenvolvimento de Software",
+      "detalhes":[
+        "Implementação e testes de novas ferramentas para software (PHP, Jquery, Ajax, CSS, Html)",
+        "Desenvolvimento e implementação de website na plataforma wordpress"
+      ]
     }
   ]
