0762f9ee5dce00023335279205632c2fddef7039
rarma4 <rarma4@gmail.com>
2024-02-07 23:58:11 -0300

    atualizando sitemap

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://www.rafart.com.br/',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'daily',
       priority: 1,
     }
   ]
