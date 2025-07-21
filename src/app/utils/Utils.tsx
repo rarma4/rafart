062580a5dc3746bae1209420e88fe4e702d37026
rarma4 <rarma4@gmail.com>
2025-07-21 14:14:20 -0300

    atualização chamada experiencia

--- src/app/utils/Utils.tsx
+++ src/app/utils/Utils.tsx
@@ -14,7 +14,7 @@ export const FormacaoAcademica = [
       "retorno":"one",
       "nome": "Tenda Negócios Imobiliários",
       "data": "10/2021 – 07/2025",
-      "descricao": "Programador de Internet Web III",
+      "descricao": "Desenvolvedor Pleno/Senior Frontend",
       "detalhes":[
         "React/NextJs/NodeJs - Implementação e testes A/B ,aplicações, desenvolvimento e manutenção de ferramentas, Comunicação AWS S3, Implementações de Designer  sistems;",
         "PHP Wordpress - Implementação e testes A/B, Criação de plugin para salvar Lead, criação de tema personalizado, Aplicação SEO para melhor desempenho Pagespeed, aplicações, desenvolvimento e manutenção de ferramentas;",
